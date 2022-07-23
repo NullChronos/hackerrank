bf9bd24075957601ff6f64e3e1519c0f5b5dbc3d
Null Chronos <nullchronos@outlook.com>
2022-07-23 14:29:25 -0400

    Day 1: Let and Const

---
+++ 10_days_of_javascript/day_1_let_and_const.js
@@ -0,0 +1,12 @@
+function main() {
+  const PI = Math.PI
+  const r = readLine()
+
+  let a = PI * Math.pow(r, 2)
+  let p = 2 * PI * r
+
+  // Print the area of the circle:
+  console.log(a)
+  // Print the perimeter of the circle:
+  console.log(p)
+}
